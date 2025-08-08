35cfc8e9ffa37d72f3db09c74b75525ba946e3dd
Mirek Długosz <mirek@mirekdlugosz.com>
2025-08-09 00:42:33 +0200

    Introduce UI thread, better cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::time::{Duration, Instant};
 use std::thread::{self, JoinHandle};
 
 use argh::FromArgs;
-use crossbeam_channel::{unbounded, Sender};
+use crossbeam_channel::{self, Sender, Receiver};
 use crossterm::style::Print;
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::{cursor, ExecutableCommand, QueueableCommand};
@@ -42,28 +42,32 @@ struct SnoozeArgs {
 
 
 enum SnoozeMessage {
-    USR1,
+    PrintTime,
+    Terminate(i32),
 }
 
 
-fn install_signal_handlers(sender: Sender<SnoozeMessage>) -> Option<(Handle, JoinHandle<()>)> {
+fn install_signal_handlers(
+    loop_sender: Sender<SnoozeMessage>,
+    ui_sender: Sender<SnoozeMessage>
+) -> Option<(Handle, JoinHandle<()>)> {
     let known_signals = [
         signal::SIGUSR1,
         signal::SIGTERM,
         signal::SIGQUIT,
         signal::SIGINT,
     ];
-    let mut signals = Signals::new(&known_signals).ok()?;
+    let mut signals = Signals::new(known_signals).ok()?;
     let handle = signals.handle();
     let thread = thread::spawn(move || {
         for signal in &mut signals {
             match signal {
                 signal::SIGUSR1 => {
-                    let _ = sender.send(SnoozeMessage::USR1);
+                    let _ = ui_sender.send(SnoozeMessage::PrintTime);
                 },
                 signal::SIGTERM | signal::SIGQUIT | signal::SIGINT => {
-                    println!("super signal!");
-                    let _ = low_level::emulate_default_handler(signal);
+                    let _ = ui_sender.send(SnoozeMessage::Terminate(signal));
+                    let _ = loop_sender.send(SnoozeMessage::Terminate(signal));
                 }
                 _ => (),
             }
@@ -72,6 +76,28 @@ fn install_signal_handlers(sender: Sender<SnoozeMessage>) -> Option<(Handle, Joi
     Some((handle, thread))
 }
 
+
+fn start_ui(end_time: Instant, ui_receiver: Receiver<SnoozeMessage>) -> JoinHandle<()> {
+    let mut stdout = stdout();
+    thread::spawn(move || {
+        loop {
+            match ui_receiver.recv() {
+                Ok(SnoozeMessage::Terminate(_)) | Err(_) => break,
+                Ok(SnoozeMessage::PrintTime) => {
+                    let remaining = end_time - Instant::now();
+                    stdout
+                        .queue(Clear(ClearType::CurrentLine)).unwrap()
+                        .queue(cursor::Hide).unwrap()
+                        .queue(cursor::MoveToColumn(0)).unwrap()
+                        .queue(Print(format!("Left: {remaining:?}"))).unwrap()
+                        .flush().unwrap();
+                }
+            }
+        }
+        stdout.execute(cursor::Show).unwrap();
+        println!();
+    })
+}
 
 // FIXME: jakie errory zwraca sleep?
 #[repr(u8)]
@@ -109,39 +135,46 @@ fn main() -> SnoozeResult {
 
     // jakaś logika, że mniej niż sekunda to po prostu śpimy z quiet
 
-    let (s, r) = unbounded();
-    let mut stdout = stdout();
+    let (loop_sender, loop_receiver) = crossbeam_channel::unbounded();
+    let (ui_sender, ui_receiver) = crossbeam_channel::unbounded();
 
-    let Some((signals_handle, signals_thread)) = install_signal_handlers(s.clone()) else {
+    let Some((signals_handle, signals_thread)) = install_signal_handlers(loop_sender, ui_sender.clone()) else {
         println!("Couldn't create signal handlers");
         return SnoozeResult::OsError;
     };
 
+    let ui_thread = start_ui(end_time, ui_receiver);
+
+    let mut close_signal: Option<i32> = None;
+
     loop {
+        match loop_receiver.try_recv() {
+            Ok(SnoozeMessage::Terminate(signal_)) => {
+                close_signal = Some(signal_);
+                break;
+            },
+            Ok(SnoozeMessage::PrintTime) => (),
+            // FIXME: ok to ignore errors?
+            Err(_) => (),
+        }
         let remaining = end_time - Instant::now();
         if remaining.is_zero() {
             break;
         }
-        let handle_usr1 = match r.try_recv() {
-            Ok(_) => true,
-            Err(_) => false,
-        };
-        if !parsed_args.quiet || handle_usr1 {
-            stdout
-                .queue(Clear(ClearType::CurrentLine)).unwrap()
-                .queue(cursor::Hide).unwrap()
-                .queue(cursor::MoveToColumn(0)).unwrap()
-                .queue(Print(format!("Left: {remaining:?}"))).unwrap()
-                .flush().unwrap();
+        if !parsed_args.quiet {
+            let _ = ui_sender.try_send(SnoozeMessage::PrintTime);
         }
         thread::sleep(remaining.min(REFRESH_TIME));
     }
 
-    stdout.execute(cursor::Show).unwrap();
-    println!();
+    let _ = ui_sender.send(SnoozeMessage::Terminate(0));
 
     signals_handle.close();
+    ui_thread.join().unwrap();
     signals_thread.join().unwrap();
+    if let Some(signal_) = close_signal {
+        let _ = low_level::emulate_default_handler(signal_);
+    }
 
     SnoozeResult::Good
 }
